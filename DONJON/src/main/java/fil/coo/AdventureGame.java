2abccfb0cca77ecec71672de7467b416a2b04dbc
hypsnowfrog <hypsnowfrog@hypsnowfrog.pc>
2019-10-01 10:22:29 +0200

    feat: main class AdventureGame added

---
+++ DONJON/src/main/java/fil/coo/AdventureGame.java
@@ -0,0 +1,15 @@
+package fil.coo;
+/**
+ * Main class of the project.
+ * AdventureGame handle the main game mechanics such as 
+ * 
+ *
+ */
+public class AdventureGame {
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+}
