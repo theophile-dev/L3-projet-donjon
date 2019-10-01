08e0c833495edcec96a51cdf5835cc2becf02536
covey <florian.covey.etu@univ-lille.fr>
2019-10-01 10:59:00 +0200

    feat: ajout Class Abstract Item avec Class Purse, Heal et Force

---
+++ DONJON/src/main/java/fil/coo/Heal.java
@@ -0,0 +1,13 @@
+package fil.coo;
+
+public class Heal extends Item {
+	private int pv;
+	
+	
+	@Override
+	public void use() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DONJON/src/main/java/fil/coo/Purse.java
@@ -0,0 +1,12 @@
+package fil.coo;
+
+public class Purse extends Item {
+	private int po;
+		
+	@Override
+	public void use() {
+	// Player.setPo(Player.getPo + po;	
+
+	}
+
+}
